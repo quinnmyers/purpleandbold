6ffc40f6b042bae753443d3aa60e3fb65f247cef
Quinn <quinnsmyers@gmail.com>
2019-01-12 22:24:13 -0800

    reverted back to 9a54b1 before merge

--- purpleandbold/src/pages/index.js
+++ purpleandbold/src/pages/index.js
@@ -18,7 +18,7 @@ const IndexPage = () => (
       <Hero />
       <Services />
       {/* <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} /> */}
-      <h1>Hi people</h1>
+      <h1>Hi people back to before merge</h1>
       {/* <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
