3783f6f46f018a37b5e9c8d6fa8cbe6cfcd8c049
Quinn <quinnsmyers@gmail.com>
2019-01-12 22:27:26 -0800

    reverted back to 9a54b41

--- purpleandbold/src/pages/index.js
+++ purpleandbold/src/pages/index.js
@@ -8,11 +8,6 @@ import SEO from '../components/seo'
 //components
 import Landing from '../components/Landing/Landing'
 import Hero from '../components/Hero/Hero'
-
-import Contact from '../components/Contact/Contact'
-import About from '../components/About/About'
-import Testimonials from '../components/Testimonials/Testimonials'
-
 import Services from '../components/Services/Services'
 import Contact from '../components/Contact/Contact'
 
@@ -23,7 +18,7 @@ const IndexPage = () => (
       <Hero />
       <Services />
       {/* <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} /> */}
-      <h1>Hi people back to before merge</h1>
+      <h1>Hi people</h1>
       {/* <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
