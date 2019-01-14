3cc90cc36439bbcbe90621b71707f4ff7e1f2f51
cole smith <colesmith@Coles-iMac.local>
2019-01-13 23:36:26 -0700

    added about panles

--- purpleandbold/src/components/About/About.js
+++ purpleandbold/src/components/About/About.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import AboutPanels from "./AboutPanles"
 import style from "./about.module.sass"
+import brand from "../../images/brand.svg"
 class About extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            isInView: false
+        }
+        this.aboutBody = React.createRef()
+        this.isInViewport = this.isInViewport.bind(this)
     }
+    isInViewport(offset = 0) {
+        if (!this.yourElement) return false;
+        const top = this.aboutBody.current.getBoundingClientRect().top;
+        return (top + offset) >= 0 && (top - offset) <= window.innerHeight;
+    }
+
+
     render() {
         return (
-            <div>
+            <div ref={this.aboutBody}>
                 <div className={style.about} id="about">
                     <div className={style.about__content}>
                         <div className={style.about__content__container}>
                             <div className={style.about__content__container__left}>
-                                <AboutPanels></AboutPanels>
+                                <AboutPanels inview={this.state.isInView}></AboutPanels>
                             </div>
                             <div className={style.about__content__container__right}>
-                                <img src="../../assets/images/brand.svg" alt="Purple and Bold Logo" className={style.about__content__container__right__image} />
+                                <img src={brand} alt="Purple and Bold Logo" className={style.about__content__container__right__image} />
                                 <p>Starting a business or building a brand can be overwhelming. Why? So many steps must be taken before you can even start: you need a logo, you need a website,  you don’t even have a name or brand, a business card is crucial, you also need to show up on Google, where do you start?  At Purple and Bold we strive to be your partner and handle those tasks for you, the right way,  so you can focus on what really matters, your business and life. We enjoy and obsess over the large and small details of a business and brand and know that attention to detail early on in the process of opening a business or revamping one can make a dramatic difference later.  Purple and Bold’s goal is to provide everything our clients need to start their next venture, or revamp one that needs a little attention. We are committed to providing quality work, clear and frequent communication, and delivering quality products on time, after all, we want to do this for a long time,  it’s what we love.</p>
                             </div>
                         </div>
--- purpleandbold/src/components/About/AboutPanles.js
+++ purpleandbold/src/components/About/AboutPanles.js
@@ -1,14 +1,146 @@
 import React, { Component } from 'react';
+import iso1 from "../../images/hero-iso/iso-1.svg"
+import iso2 from "../../images/hero-iso/iso-2.svg"
+import iso3 from "../../images/hero-iso/iso-3.svg"
+import iso4 from "../../images/hero-iso/iso-4.svg"
+import iso5 from "../../images/hero-iso/iso-5.svg"
+import iso6 from "../../images/hero-iso/iso-6.svg"
+import iso7 from "../../images/hero-iso/iso-7.svg"
+import iso8 from "../../images/hero-iso/iso-8.svg"
+
+import "./aboutpanles.sass"
 // import hero from '../hero'
 class About extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            heroImages: [
+                {
+                    src: iso1
+                },
+                {
+                    src: iso2
+                },
+                {
+                    src: iso3
+                },
+                {
+                    src: iso4
+                },
+                {
+                    src: iso5
+                },
+                {
+                    src: iso6
+                },
+                {
+                    src: iso7
+                },
+                {
+                    src: iso8
+                }
+            ],
+            panelArray: [],
+            display: "none",
+            panelLoaded: "",
+            loadIn: "",
+            aboutImage: "",
+            firsClass: false,
+            secondClass: false
+        }
+        this.styleAboutListItem = this.styleAboutListItem.bind(this)
+        this.getRandomNumber = this.getRandomNumber.bind(this)
+        this.panelAnimations = this.panelAnimations.bind(this)
+        this.aboutBody = React.createRef()
+        this.isInViewport = this.isInViewport.bind(this)
     }
+
+    isInViewport(offset = -200) {
+        if (!this.yourElement) return false;
+        const top = this.aboutBody.current.getBoundingClientRect().top;
+        return (top + offset) >= 0 && (top - offset) <= window.innerHeight;
+    }
+
+    componentDidMount() {
+        document.addEventListener("scroll", () => {
+            if (this.isInViewport) {
+                this.setState({
+                    firsClass: true,
+                    secondClass: true
+                })
+            }
+        })
+        // this.selectListItem();
+        this.panelAnimations();
+        if (this.props.inview) {
+            this.setState({ firsClass: true })
+            this.setState({ secondClass: true })
+        }
+
+    }
+    buildRef(i) {
+        return `this.Ref${i}`
+    }
+    getImgSrc(index) {
+        return require("../../images/hero-iso/iso-" + index + ".svg");
+    }
+    stylePanel() {
+        //translateX
+        const minTranslateX = 100;
+        const maxTranslateX = 800;
+        let translateX = this.getRandomNumber(minTranslateX, maxTranslateX);
+        if (Math.random() > 0.5) {
+            translateX = translateX * -1;
+        }
+        //translateY
+        //multiplied by 0.57 to ensure the deg movement is 30 degree the same angle used in isometric illustrations
+        const translateY = translateX * 0.57;
+        //scale
+        const scale = Math.random();
+        return {
+            transform: `scale(${scale}) translateX(${translateX}px) translateY(${translateY}px) rotateY(180deg)`
+            //may want to add in rotation to styling
+            //
+        };
+    }
+    styleAboutListItem() {
+        // const minScale = 0.97;
+        // const maxScale = 1.03;
+        //const scale = this.getRandomNumber(minScale, maxScale);
+        const minHeightOffset = 2;
+        const maxHeightOffset = 9;
+        let heightOffset = this.getRandomNumber(minHeightOffset, maxHeightOffset);
+        if (Math.random() > 0.5) {
+            heightOffset = heightOffset * -1;
+        }
+        return {
+            marginTop: `${heightOffset}px`
+        };
+    }
+
+    //constrols starts of animation and timeout of others if added
+    panelAnimations() {
+        this.state.loadIn = "load__in";
+        this.state.aboutImage = "about__image";
+        this.state.panelLoaded = "panel__loaded";
+    }
+
+    getRandomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+
+
     render() {
         return (
-            <div>
-                <h1>I need a hero</h1>
+            <div className="wrapper" ref={this.aboutBody}>
+                <div className="not__ul">
+                    {this.state.heroImages.map((heroImage, index) => (
+                        <div key={index} className="not__li">
+                            <img v-in-viewport src={this.getImgSrc(index + 1)} className={`about__panel__image__${index + 1} ${this.state.firsClass ? "load__in" : " "}  ${this.state.secondClass ? "panel__loaded" : " "}`} ref={this.buildRef(index)} alt="Vector illustration of of a square" style={this.stylePanel(index)} />
+                        </div>
+                    ))}</div>
+
             </div>
         );
     }
