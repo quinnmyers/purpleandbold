1118a3aac7b72aaff2b68e6156c4b72568fedf35
Quinn <quinnsmyers@gmail.com>
2019-01-14 23:15:31 -0800

    improved styling of desktop hero

--- purpleandbold/src/components/Hero/Hero.js
+++ purpleandbold/src/components/Hero/Hero.js
@@ -1,48 +1,50 @@
 import React from 'react'
 import style from './hero.module.sass'
-import heroMonitor from "../../images/hero-monitor-2.svg"
+import heroMonitor from '../../images/hero-monitor-2.svg'
+
+//components
+import Content from '../utility/Content/Content'
+
 class hero extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    this.state = {
-      isMounted: false,
-      aboutSlides: [
-        "creative agency",
-        "web development firm",
-        "branding company",
-        "design agency"
-      ],
-      lineBreak: false
-    }
-
-  };
+    this.state = {}
+  }
 
   render() {
     return (
-      <div >
+      <div>
         <section className={style.hero} id="hero">
           <div className={style.hero__content}>
-            <div className={style.hero__content__container}>
-              <h3>A modern
-                  </h3>
-              <h3>
-                If you're starting something new,
-                let's make sure it's not already old.
-                  </h3>
-              <a href="#contact" >
-                <button class={style.hero__content__container__ctabutton}>
-                  Get A Free Quote</button>
-              </a>
-            </div>
+            <Content>
+              <div className={style.hero__content__container}>
+                <div className={style.hero__content__container__top}>
+                  <h4>A modern solution to everything digital.</h4>
+                </div>
+                <div className={style.hero__content__container__bottom}>
+                  <h4>
+                    If you're starting something new, let's make sure it's not
+                    already old.
+                  </h4>
+                  <a href="#contact">
+                    <button class={style.hero__content__container__ctabutton}>
+                      Get A Free Quote
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </Content>
           </div>
           <div className={style.hero__image__container}>
-            <img src={heroMonitor} alt="Vector illustration of computer monitor" />
+            <img
+              src={heroMonitor}
+              alt="Vector illustration of computer monitor"
+            />
           </div>
         </section>
       </div>
     )
-
   }
 }
 export default hero
