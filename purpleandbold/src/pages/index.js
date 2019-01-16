41e2b926c2b7834d7b290343540d1dfd4c48e071
Quinn <quinnsmyers@gmail.com>
2019-01-15 21:44:01 -0800

    commit before merge

--- purpleandbold/src/pages/index.js
+++ purpleandbold/src/pages/index.js
@@ -12,7 +12,7 @@ import Services from '../components/Services/Services'
 import Contact from '../components/Contact/Contact'
 import About from '../components/About/About'
 import Testimonials from '../components/Testimonials/Testimonials'
-import Portfolio from "../components/Portfolio/Portfolio"
+import Portfolio from '../components/Portfolio/Portfolio'
 
 const IndexPage = () => (
   <div>
@@ -22,16 +22,8 @@ const IndexPage = () => (
       <About />
       <Services />
       {/* <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} /> */}
-      <h1>Hi people</h1>
-      {/* <p>Welcome to your new Gatsby site.</p>
-      
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> */}
       <Testimonials />
-      <Portfolio></Portfolio>
+      <Portfolio />
       <Contact />
     </Layout>
   </div>
