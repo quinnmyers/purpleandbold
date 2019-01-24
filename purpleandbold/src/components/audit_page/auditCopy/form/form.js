620fba15153133c9bbc93564aeec41a90c065c07
cole smith <colesmith@Coles-iMac.local>
2019-01-23 22:05:43 -0700

    made new A conponent

--- purpleandbold/src/components/audit_page/auditCopy/form/form.js
+++ purpleandbold/src/components/audit_page/auditCopy/form/form.js
@@ -1,59 +1,64 @@
 import React, { Component } from 'react'
 import './form.sass'
 class Form extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-  render() {
-    return (
-      <div className="form">
-        <form
-          method="POST"
-          action="https://formfor.site/send/nHLl9Gl7zttcbnyXbjDB4WGA2Hxzxq"
-        >
-          <div className="form__small">
-            <label htmlFor="form__first">First Name</label>
-            <input type="text" name="first" id="form__first" />
-          </div>
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="form">
+                <form
+                    method="POST"
+                    action="https://formfor.site/send/nHLl9Gl7zttcbnyXbjDB4WGA2Hxzxq"
+                >
+                    <div className="form__small">
+                        <label htmlFor="form__first">First Name</label>
+                        <input type="text" name="first" id="form__first" />
+                    </div>
 
-          <div className="form__small">
-            <label htmlFor="form__last">Last Name</label>
-            <input type="text" name="last" id="form__last" />
-          </div>
+                    <div className="form__small">
+                        <label htmlFor="form__last">Last Name</label>
+                        <input type="text" name="last" id="form__last" />
+                    </div>
 
-          <div className="form__small">
-            <label htmlFor="form__business">Name of Business</label>
-            <input type="text" name="business" id="form__business" />
-          </div>
+                    <div className="form__small">
+                        <label htmlFor="form__business">Name of Business</label>
+                        <input type="text" name="business" id="form__business" />
+                    </div>
 
-          <div className="form__small">
-            <label htmlFor="form__phone">Phone Number</label>
-            <input type="text" name="phone" id="form__phone" />
-          </div>
+                    <div className="form__small">
+                        <label htmlFor="form__phone">Phone Number</label>
+                        <input type="text" name="phone" id="form__phone" />
+                    </div>
 
-          <div className="form__small">
-            <label htmlFor="form__website">Business Website URL</label>
-            <input type="text" name="website" id="form__website" />
-          </div>
+                    <div className="form__small">
+                        <label htmlFor="form__website">Business Website URL</label>
+                        <input type="text" name="website" id="form__website" />
+                    </div>
 
-          <div className="form__small">
-            <label htmlFor="form__email">Email To Deliver Audit</label>
-            <input type="email" name="email" id="form__email" />
-          </div>
-          <div className="form__large">
-            <label htmlFor="form__goals">
-              Tell Us A Bit About Your Business and Goals
+                    <div className="form__small">
+                        <label htmlFor="form__email">Email To Deliver Audit</label>
+                        <input type="email" name="email" id="form__email" />
+                    </div>
+                    <div className="form__large">
+                        <label htmlFor="form__goals">
+                            Tell Us A Bit About Your Business and Goals
             </label>
-            <textarea name="goals" id="form__goals" cols="30" rows="10" />
-          </div>
-          <div className="button__container">
-            <button type="submit">Submit Your Request</button>
-          </div>
-        </form>
-      </div>
-    )
-  }
+                        <textarea name="goals" id="form__goals" cols="30" rows="10" />
+                    </div>
+                    <div className="button__container">
+                        <input
+                            type="hidden"
+                            name="_success"
+                            value="https://purpleandbold.com/request-audit/#submitted"
+                        />
+                        <button type="submit">Submit Your Request</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Form
---
+++ purpleandbold/src/components/utility/link/A.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+class A extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHome: false,
+            homePath: ""
+        }
+
+    }
+    componentDidMount() {
+        // console.log(window.location);
+
+        if (window.location.pathname === "/") {
+            this.setState({ isHome: true })
+        }
+        this.setState({ homePath: window.location.origin })
+
+    }
+    render() {
+        return (
+            this.state.isHome ?
+                (<AnchorLink href={this.props.href} offset="70">{this.props.children}</AnchorLink>)
+                :
+                (<a href={`${this.state.homePath}/${this.props.href}`}>{this.props.children}</a>)
+
+        );
+    }
+}
+
+export default A;
