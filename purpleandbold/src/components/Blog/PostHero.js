aa79ff3b4777a4a3be16ee2dc0ad8f0562627d45
Quinn <quinnsmyers@gmail.com>
2019-02-08 20:46:53 -0800

    styled blog index for desktop and added all other elements

--- purpleandbold/src/components/Blog/PostHero.js
+++ purpleandbold/src/components/Blog/PostHero.js
@@ -1,30 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Img from "gatsby-image"
-
+import Img from 'gatsby-image'
+import { Link } from 'gatsby'
 
 //styles
 import './posthero.sass'
 
-const PostHero = ({ img, title, desc, author, date }) => (
-  <div className="posthero">
-    <div className="posthero__info">
-      <div className="posthero__info__top">
-        <h2>{title}</h2>
+const PostHero = ({ slug, img, title, desc, author, date }) => (
+  <Link to={slug}>
+    <div className="posthero">
+      <div className="posthero__info">
+        <div className="posthero__info__top">
+          <h2>{title}</h2>
+        </div>
+        <div className="posthero__info__bottom">
+          <p>{desc}</p>
+          <p>
+            <span>
+              {author}, &nbsp; {date}
+            </span>
+          </p>
+        </div>
       </div>
-      <div className="posthero__info__bottom">
-        <p>{desc}</p>
-        <p>
-          <span>
-            {author}, &nbsp; {date}
-          </span>
-        </p>
+      <div className="posthero__image">
+        <img src={img} alt="" />
       </div>
     </div>
-    <div className="posthero__image">
-      <img src={img} alt="" />
-    </div>
-  </div>
+  </Link>
 )
 
 export default PostHero
--- purpleandbold/src/pages/blog.js
+++ purpleandbold/src/pages/blog.js
@@ -13,7 +13,7 @@ import PostHero from '../components/Blog/PostHero'
 import AnalFeatured from '../images/blog/analytics/blog-analytics-noreflect.jpg'
 
 //styles
-import '../components/blog/blogindex.sass'
+import '../components/Blog/blogindex.sass'
 
 class Blog extends Component {
   constructor(props) {
@@ -25,37 +25,58 @@ class Blog extends Component {
     return (
       <Layout>
         <Content>
-          {data.map(post => (
-            <div className="post" key={post.id}>
-              <PostPreview
-                img={post.node.frontmatter.featuredImage.childImageSharp.fluid}
-                title={post.node.frontmatter.title}
-                desc={post.node.frontmatter.exceprt}
+          <div className="blogindex">
+            <div className="blogindex__header">
+              <h2>Welcome to Our Blog</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.{' '}
+              </p>
+            </div>
+            <div className="blogindex__featured">
+              <h4>Featured Post</h4>
+              <PostHero
+                img={AnalFeatured}
+                title="Analytics In Modern Websites"
+                desc={`Learn what analytics are, how to use analytics, and why analytics are important to your business' success`}
                 author={`Quinn Myers`}
                 date={`2/6/19`}
+                slug={`#`}
               />
-              {/* <Img
-                fluid={
-                  post.node.frontmatter.featuredImage.childImageSharp.fluid
-                }
-              />
-              <h2>{post.node.frontmatter.title}</h2>
-              <p>{post.node.frontmatter.exceprt}</p> */}
             </div>
-          ))}
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <div className="test__components">
-            <PostHero
-              img={AnalFeatured}
-              title="Analytics In Modern Websites"
-              desc={`Learn what analytics are, how to use analytics, and why analytics are important to your business' success`}
-              author={`Quinn Myers`}
-              date={`2/6/19`}
-            />
+            <div className="blogindex__posts">
+              <h4>All Blog Posts</h4>
+              <div className="blogindex__posts__filters">
+                <div className="blogindex__posts__filters__buttons">
+                  <h5>Categories:</h5>
+                  <button>websites</button>
+                  <button>marketing</button>
+                  <button>design</button>
+                </div>
+                <div className="blogindex__posts__filters__search">
+                  <img src="http://placehold.it/35x35" alt="" />
+                  <input type="text" />
+                </div>
+              </div>
+            </div>
+            <div className="blogindex__posts__grid">
+              {data.map(post => (
+                <div className="post" key={post.id}>
+                  <PostPreview
+                    img={
+                      post.node.frontmatter.featuredImage.childImageSharp.fluid
+                    }
+                    title={post.node.frontmatter.title}
+                    desc={post.node.frontmatter.exceprt}
+                    author={`Quinn Myers`}
+                    date={`2/6/19`}
+                    slug={post.node.fields.slug}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </Content>
       </Layout>
