3dae7e4352bea585580ea50fc8983527d4a7c329
cole smith <colesmith@Coles-iMac.local>
2019-02-17 22:23:57 -0700

    bugfix

--- purpleandbold/src/components/utility/Button/Button.js
+++ purpleandbold/src/components/utility/Button/Button.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactGA from 'react-ga'
+import { Link } from "gatsby"
 
 //components
 import AnchorLink from 'react-anchor-link-smooth-scroll'
@@ -10,11 +11,16 @@ import './button.sass'
 
 //section must be in ID format, like "#contact" for Contact Section
 //text is inner text of button
-const Button = ({ section, text, loadButton }) => (
+const Button = ({ section, text, loadButton, noscroll }) => (
   <div className={`button ${loadButton ? 'loaded' : ''}`}>
-    <AnchorLink href={section} offset="70">
+    {noscroll ? (<Link to={section}>
       <div className={`spacer ${loadButton ? 'loaded' : ''}`}>{text}</div>
-    </AnchorLink>
+    </Link>) : (
+        <AnchorLink href={section} offset="70">
+          <div className={`spacer ${loadButton ? 'loaded' : ''}`}>{text}</div>
+        </AnchorLink>
+      )}
+
   </div>
 )
 
