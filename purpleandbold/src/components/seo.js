c24655562cf501a4842cc2cf5f2c05b206bada52
Quinn <quinnsmyers@gmail.com>
2019-03-09 20:17:23 -0800

    fixed build errors

--- purpleandbold/src/components/seo.js
+++ purpleandbold/src/components/seo.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import ogImage from 'ogImage.png'
+import ogImage from './ogImage.png'
 
 function SEO({ description, lang, meta, keywords, title, page }) {
   return (
